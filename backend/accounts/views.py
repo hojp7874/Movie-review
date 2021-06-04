5941225995c0d6048bf24cff4fc7a407c11dbb1a
Jinpyo Hong <hojp7874@gmail.com>
2021-06-04 20:06:48 +0900

    refector: clean code

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -19,14 +19,11 @@ def profile(request):
     return Response(serializer.data)
 
 
-
 # 유저정보 가져오기
 @api_view(['GET'])
 def get_users(request):
-    # users = get_user_model()
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    print('serializer complete')
     return Response(serializer.data)
 
 
@@ -49,22 +46,18 @@ def signup(request):
         #4. 비밀번호 해싱 후 
         user.set_password(request.data.get('password'))
         user.save()
-        # print(user.password)
     # password는 직렬화 과정에는 포함 되지만 → 표현(response)할 때는 나타나지 않는다.
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['PUT', 'DELETE'])
 def user_update_delete(request):
-    print('call')
     if request.method == 'PUT':
-        print('PUT')
         #1-1. Client에서 온 데이터를 받아서
         old_password = request.data.get('oldPassword')
             
         #1-2. 패스워드 일치 여부 체크
         if old_password != request.user.password:
-            print(request.user.password)
             return Response({'error': '이전 비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
             
         #2. UserSerializer를 통해 데이터 직렬화
@@ -76,11 +69,9 @@ def user_update_delete(request):
             #4. 비밀번호 해싱 후 
             user.set_password(request.data.get('newPassword'))
             user.save()
-            # print(user.password)
         # password는 직렬화 과정에는 포함 되지만 → 표현(response)할 때는 나타나지 않는다.
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
-        print('DELETE')
         user = User.objects.get(pk=request.user)
         user.delete()
         return Response({'id가 삭제되었습니다'})
